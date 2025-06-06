e190ff561a6544aa80850186b55664bfad943ad2
Pichaku123 <pribisht785@gmail.com>
2025-06-06 13:32:09 +0530

    equals button works+takes multiple inputs

--- scripts.js
+++ scripts.js
@@ -25,27 +25,89 @@ function division(a,b){
 function operate(a, operator, b){
     switch(operator)
     {
-        case "+": addition(a,b); 
-        break;
-        case "-": subtraction(a,b); 
-        break;
-        case "*": multiplication(a,b); 
-        break;
-        case "/": division(a,b); 
-        break;
+        case "+": return addition(Number(a),Number(b)); 
+        case "-": return subtraction(Number(a),Number(b)); 
+        case "*": return multiplication(Number(a),Number(b)); 
+        case "/": return division(Number(a),Number(b)); 
     }
 }
 
-const display_keys=document.querySelectorAll(".digits, .operators");
+const digits=document.querySelectorAll(".digits");
+const operators=document.querySelectorAll(".operators");
 const display=document.querySelector("#display");
-let ip=""; 
+const equals=document.querySelector("#equals");
+let ip="";
+let a="", b="", oper="";
+let firstOperatorUsed=false, secondOperatorUsed=false; 
 
-display_keys.forEach((key) => {
-    key.addEventListener("click", () =>{
-        ip+=key.textContent;
-        display.textContent=`${ip}`;
-        console.log(ip);
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        if(!firstOperatorUsed){
+            a+=digit.textContent;
+            console.log(a);
+        }
+        else if(firstOperatorUsed){
+            b+=digit.textContent;
+            console.log(b);
+        }
     });
 });
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if(!firstOperatorUsed){  //an oper clicked when none have been clicked yet
+            oper=operator.textContent;
+            firstOperatorUsed=true;
+            console.log(`first op= ${oper}`);
+        }
+        else if(firstOperatorUsed){
+            a=String(operate(a,oper,b));
+            b="";
+            oper=operator.textContent;
+            console.log(`new a=${a}, oper=${oper}`);
+        }
+    }); 
+});
+
+equals.addEventListener("click", () => {
+    console.log(`result= ${String(operate(a,oper,b))}`);
+});
+// function splitter(ip){
+//     console.log(`input is ${ip}`);  //working fine
+//     //we need to split the function, so that it has 1 number, 1 operator and another number.
+//     //to accomodate for multiple operators, we need to stop the first operation after we find 2nd oper.
+//     let foundFirstOperator=false, foundSecondOperator=false;
+//     let a="", b="";
+//     while(!foundSecondOperator)
+//     {
+//         for(let i=0; i<ip.length; i++)
+//         {
+            
+//         }
+//     }
+// }
+
+// display_keys.forEach((key) => {
+//     key.addEventListener("click", () =>{
+        
+//         if(key.classList.contains("operators") && !operatorUsed){   
+//             //if key is operator and operator is not used yet
+//             ip+=key.textContent;
+//             display.textContent=`${ip}`;
+//             operatorUsed=true;
+//             console.log(ip);
+//         }
+//         else if(key.classList.contains("operators") && operatorUsed){   
+//             //if key is operator and operator has been used
+//             splitter(ip);
+//             console.log(ip);
+//         }
+
+//     });
+// });
+
+// equals.addEventListener("click", () => {
+//     splitter(ip);
+// });
 
 
